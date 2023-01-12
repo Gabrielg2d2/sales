fac32704807291893a0e6e455d78bfe1013aa260
G2D2 <gabriel.g2d2@gmail.com>
2023-01-12 20:41:36 -0300

    feat: 🎸 mock sales and list sales

---
+++ src/mock/sales.ts
@@ -0,0 +1,27 @@
+export const salesMock = {
+  id: 'id_fake',
+  status: 'status_fake',
+  origin: 'origin_fake',
+  dateAndTime: '2019-01-01T00:00:00.000Z',
+  flag: 'flag_fake',
+  code: 'code_fake',
+  brute: 1000,
+  liquid: 700,
+  paymentMethod: 'paymentMethod_fake',
+  storeOrDocument: 'storeOrDocument_fake'
+}
+
+export const salesMockTwo = {
+  id: 'id_fake2',
+  status: 'status_fake2',
+  origin: 'origin_fake2',
+  dateAndTime: '2019-01-01T00:00:00.000Z',
+  flag: 'flag_fake2',
+  code: 'code_fake2',
+  brute: 1000,
+  liquid: 700,
+  paymentMethod: 'paymentMethod_fake2',
+  storeOrDocument: 'storeOrDocument_fake2'
+}
+
+export const salesMockList = [salesMock, salesMockTwo]
