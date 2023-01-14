e4fb9a1e9a5243d49277102864cd7a3e17c746b1
G2D2 <gabriel.g2d2@gmail.com>
2023-01-13 21:40:05 -0300

    feat: 🎸 class main sales

---
+++ src/data-layer/sales/main.ts/index.ts
@@ -0,0 +1,26 @@
+import { ListSales, FormatListSales } from 'data-layer/sales/use_cases'
+import { DataSalesModelFormatted } from 'domain/types'
+import { api } from 'service/api'
+
+interface ISalesMain {
+  getSales: () => Promise<DataSalesModelFormatted[]>
+}
+
+export class SalesMain implements ISalesMain {
+  async getSales() {
+    try {
+      const listSales = new ListSales('/sales', api.get)
+      const { data, status } = await listSales.list()
+
+      if (status === 200) {
+        const formatListSales = new FormatListSales()
+        const dataFormatted = formatListSales.format(data)
+        return dataFormatted
+      }
+
+      return []
+    } catch (error) {
+      return []
+    }
+  }
+}
