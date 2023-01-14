57dacff8b54aa281c827c13414e90e393e7d74b8
G2D2 <gabriel.g2d2@gmail.com>
2023-01-14 09:15:04 -0300

    feat: 🎸 color global

---
+++ src/global/styles/colors/index.ts
@@ -0,0 +1,32 @@
+export const globalColors = {
+  green: {
+    200: '#80cbc4',
+    400: '#26a69a',
+    600: '#00897b',
+    900: '#004d40'
+  },
+
+  blue: {
+    50: '#e3f2fd'
+  },
+
+  red: {
+    100: '#ffcdd2',
+    800: '#c62828'
+  },
+
+  orange: {
+    200: '#ffcc80',
+    700: '#f57c00'
+  },
+
+  white: {
+    50: '#ffffff',
+    100: '#fafafa'
+  },
+
+  grey: {
+    700: '#616161',
+    800: '#424242'
+  }
+} as const
