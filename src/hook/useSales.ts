89c2a5506d688bc6813f299729e82ac17be786c9
G2D2 <gabriel.g2d2@gmail.com>
2023-01-17 20:25:05 -0300

    feat: 🎸 new hook useSales

---
+++ src/hook/useSales.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from 'react'
+import { DataSalesModelFormatted } from 'domain/sales/types'
+import { SelectChangeEvent } from '@mui/material'
+import { SalesMain } from 'data-layer/sales/main'
+
+export function useSales() {
+  const [sales, setSales] = useState<DataSalesModelFormatted[]>([])
+  const [orderTable, setOrderTable] = useState('')
+  const [columns] = useState([
+    {
+      label: 'Status',
+      value: 'status'
+    },
+    {
+      label: 'Origem',
+      value: 'origin'
+    },
+    {
+      label: 'Data e hora',
+      value: 'dateAndTime'
+    },
+    {
+      label: 'Bandeira',
+      value: 'flag'
+    },
+    {
+      label: 'Código',
+      value: 'code'
+    },
+    {
+      label: 'Bruto',
+      value: 'brute'
+    },
+    {
+      label: 'Líquido',
+      value: 'liquid'
+    },
+    {
+      label: 'Modo de pagamento',
+      value: 'paymentMethod'
+    },
+    {
+      label: 'Loja/Documento',
+      value: 'storeOrDocument'
+    }
+  ])
+
+  const handleChangeOrderTable = (event: SelectChangeEvent) => {
+    setOrderTable(event.target.value)
+  }
+
+  useEffect(() => {
+    void (async () => {
+      const salesMain = new SalesMain()
+      const orderUrl = orderTable
+        ? `/sales?sortBy=${orderTable}&order=desc`
+        : '/sales?sortBy=dateAndTime&order=desc'
+      const response = await salesMain.getSales(orderUrl)
+      setSales(response)
+    })()
+  }, [orderTable])
+
+  return {
+    sales,
+    columns,
+    orderTable,
+    handleChangeOrderTable
+  }
+}
--- src/templates/Sales/index.tsx
+++ src/templates/Sales/index.tsx
@@ -1,66 +1,10 @@
-import { useEffect, useState } from 'react'
-import { SalesMain } from 'data-layer/sales/main'
-import { SelectChangeEvent, Stack } from '@mui/material'
+import { Stack } from '@mui/material'
 import { HeaderPage, InformationSectionSales, TableSales } from 'components'
-import { DataSalesModelFormatted } from 'domain/sales/types'
 import { getBrute, getLiquid, getQuantity } from 'global/functions'
+import { useSales } from 'hook/useSales'
 
 export function SalesTemplate() {
-  const [sales, setSales] = useState<DataSalesModelFormatted[]>([])
-  const [orderTable, setOrderTable] = useState('')
-  const columns = [
-    {
-      label: 'Status',
-      value: 'status'
-    },
-    {
-      label: 'Origem',
-      value: 'origin'
-    },
-    {
-      label: 'Data e hora',
-      value: 'dateAndTime'
-    },
-    {
-      label: 'Bandeira',
-      value: 'flag'
-    },
-    {
-      label: 'Código',
-      value: 'code'
-    },
-    {
-      label: 'Bruto',
-      value: 'brute'
-    },
-    {
-      label: 'Líquido',
-      value: 'liquid'
-    },
-    {
-      label: 'Modo de pagamento',
-      value: 'paymentMethod'
-    },
-    {
-      label: 'Loja/Documento',
-      value: 'storeOrDocument'
-    }
-  ]
-
-  const handleChangeOrderTable = (event: SelectChangeEvent) => {
-    setOrderTable(event.target.value)
-  }
-
-  useEffect(() => {
-    void (async () => {
-      const salesMain = new SalesMain()
-      const orderUrl = orderTable
-        ? `/sales?sortBy=${orderTable}&order=desc`
-        : '/sales?sortBy=dateAndTime&order=desc'
-      const response = await salesMain.getSales(orderUrl)
-      setSales(response)
-    })()
-  }, [orderTable])
+  const { columns, sales, orderTable, handleChangeOrderTable } = useSales()
 
   return (
     <Stack height="100vh" bgcolor="#fff" p={2} spacing={2}>
